8fd36a28cbed196f9c50e1dd650fab496ed4d6e0
Falk1n <113801587+falkones04@users.noreply.github.com>
2024-06-17 11:23:23 -0300

    progrma update

--- Program.cs
+++ Program.cs
@@ -19,7 +19,6 @@ public class Candidato
         this.notamedia = CalcularMedia();
     }
 
-    // Método para calcular a média de notas
     private float CalcularMedia()
     {
         return (this.mat + this.lin + this.red) / 3;
@@ -30,11 +29,9 @@ public class Program
 {
     public static int Comparar(Candidato x, Candidato y)
     {
-        // Compara as médias de notas (inverte a comparação para ordenar pela maior média)
         int resultado = y.notamedia.CompareTo(x.notamedia);
         if (resultado == 0)
         {
-            // Se as médias são iguais, compara as notas individuais
             resultado = y.red.CompareTo(x.red);
             if (resultado == 0)
             {
